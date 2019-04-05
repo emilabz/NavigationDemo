7ea3fb7bb9941f34f2261b135bca4fad69d07550
emilabz <emilabrahamz@gmail.com>
2019-04-05 19:11:11 -0400

    final

--- app/src/main/java/com/example/navigationdemo/secondScreen.java
+++ app/src/main/java/com/example/navigationdemo/secondScreen.java
@@ -3,11 +3,16 @@ package com.example.navigationdemo;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -28,4 +33,16 @@ public class secondScreen extends Fragment {
         return inflater.inflate(R.layout.fragment_second_screen, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnNext=getView().findViewById(R.id.btnNext);
+        final NavController navController= Navigation.findNavController(getActivity(),R.id.navhost_fragment);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.sectoThird);
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/navigationdemo/thirdFragment.java
+++ app/src/main/java/com/example/navigationdemo/thirdFragment.java
@@ -3,11 +3,16 @@ package com.example.navigationdemo;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -28,4 +33,16 @@ public class thirdFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_third, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnBack=getView().findViewById(R.id.btntoSec);
+        final NavController navController= Navigation.findNavController(getActivity(),R.id.navhost_fragment);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.thirdtoSec);
+            }
+        });
+    }
 }
